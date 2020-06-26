d13db00bc33aad42e6dc47a157323aaf4695782e
Hashi-data <shu.hashida@gmail.com>
2020-06-26 18:00:14 +0900

    実装

--- src/test/kotlin/com/example/demo/DemoApplicationTests.kt
+++ src/test/kotlin/com/example/demo/DemoApplicationTests.kt
@@ -1,11 +1,9 @@
 package com.example.demo
 
 import org.junit.jupiter.api.Test
-import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
-@SpringBootApplication
 class DemoApplicationTests {
 
 	@Test
